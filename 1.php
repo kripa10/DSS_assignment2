4f837426da3f53a5698b8ff42756de156862e96f
kripa10 <57896130+kripa10@users.noreply.github.com>
2020-01-20 20:39:04 +0545

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,33 @@
+<?php
+
+    $a=10;
+    $b=20;
+    if($a<1){
+        echo "the sum is ".($a+$b);
+    
+    }else{
+        echo 30;
+    }
+
+
+?>
+<?php
+$a=10;
+$b=30;
+if($a<1){
+    echo "the difference is ".($b-$a);
+
+}else{
+    echo 10;
+}
+?>
+<?php
+$a=10;
+$b=30;
+if($a>1){
+    echo "the product is ".($a*$b);
+
+}else{
+    echo 300;
+}
+?>
---
+++ 2.php
@@ -0,0 +1,19 @@
+<?php
+
+$a=2;
+$b=10;
+switch($a){
+    case 12: 
+        echo "sum is".($a+$b);
+    break;
+    case 20:
+        echo "mutiplication is".($a*$b);
+    break;  
+    case 8:
+        echo "difference is".($b-$a);
+    break;
+    default:
+        echo "the sum is an integer";
+}
+
+?> 
